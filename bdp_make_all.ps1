c33046899b535d594c6de6da08937865651627a2
jballar <38502081+jballar@users.noreply.github.com>
2024-04-11 18:10:03 +0200

    Added a script parameter to set the Visual Studio 2017 PATH.

--- bdp_make_all.ps1
+++ bdp_make_all.ps1
@@ -7,19 +7,26 @@
 ## LightHouse, SoftPhone, phones server 
 ##
 
+param (
+    [string]$vs2017Path = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional"
+)
+
 ## *** Bootstrap ***
 	
 cmd.exe /c .\bootstrap-vcpkg.bat
 
-##
-## Check the Visual Studio path in the triplets\x86-windows-static.cmake file
-## matches the current installation one
-##
-## Visual Studio path must be escaped correctly
-## \ -> \\
-## 
-## set(VCPKG_VISUAL_STUDIO_PATH "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional")
-##
+## Check the Visual Studio 2017 PATH
+if (-Not (Test-Path -Path $vs2017Path)) { 
+    Write-Host "Path $vs2017Path does not exists!" -ForegroundColor Red 
+    Write-Host "Please run the script passing the Visual Studio 2017 Path"
+    Write-Host "Example:"
+    Write-Host "bdp_make_all.ps1 -vs2017Path <VISUAL_STUDIO_2017_PATH>"
+    Write-Host
+    Break
+}
+
+## Set the Visual Studio 2017 PATH as environment variable
+[Environment]::SetEnvironmentVariable('VCPKG_VISUAL_STUDIO_PATH', $vs2017Path)
 
 ## *** cpprestsdk ***
 .\vcpkg.exe install cpprestsdk:x86-windows-static
