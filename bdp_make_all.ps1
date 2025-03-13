f9f946418b4536747a80dab036e04bd1663b7a62
jballar <38502081+jballar@users.noreply.github.com>
2025-03-13 15:19:23 +0100

    Refactor of build script

--- bdp_make_all.ps1
+++ bdp_make_all.ps1
@@ -7,46 +7,68 @@
 ## LightHouse, SoftPhone, phones server 
 ##
 
+### VS2017 Default Path
+### "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional",
+
+### VS2022 Default Path
+### "C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\Professional"
+
 param (
-    [string]$vs2017Path = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional",
-    [string]$vs2022Path = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\Professional"
+    [string]$vs2017Path, 
+    [string]$vs2022Path
 )
 
-## *** Bootstrap ***
-	
+######################
+### VCPKG BOOTSTRAP
+######################
+
 cmd.exe /c .\bootstrap-vcpkg.bat
 
-$vsPathExists = $false
+#################
+### ARGOMENTI ###
+#################
 
-## Check the Visual Studio 2017 PATH
-if (-Not (Test-Path -Path $vs2017Path)) { 
-    Write-Host "Path $vs2017Path does not exists!" -ForegroundColor Red 
+Write-Host "Arguments from command line"
+Write-Host
+Write-Host "VS2017 Path = $vs2017Path"
+Write-Host "VS2022 Path = $vs2022Path"
+
+####################################
+### VISUAL STUDIO PATH (CRITICAL)
+####################################
+
+### (TBR) Not really used now!!!
+[Environment]::SetEnvironmentVariable('VCPKG_VISUAL_STUDIO_PATH_VS2017', $vs2017Path)
+[Environment]::SetEnvironmentVariable('VCPKG_VISUAL_STUDIO_PATH_VS2022', $vs2022Path)
+
+$vsPathExternal = [System.Environment]::GetEnvironmentVariable('VCPKG_VISUAL_STUDIO_PATH')
+Write-Output "VCPKG_VISUAL_STUDIO_PATH (externals set) = $vsPathExternal"
+
+# Controlla altre due variabili di ambiente
+if (-not $vs2017Path -and -not $vs2022Path) {
+    Write-Output "No Visual Studio PATH passed from command line" 
+} elseif ($vs2022Path) {
+    Write-Output "VS2022: Set VCPKG_VISUAL_STUDIO_PATH = $vs2022Path" 
+    [Environment]::SetEnvironmentVariable('VCPKG_VISUAL_STUDIO_PATH', $vs2022Path)
 } else {
-    $vsPathExists = $true
+    Write-Output "VS2017: Set VCPKG_VISUAL_STUDIO_PATH = $vs2017Path" 
+    [Environment]::SetEnvironmentVariable('VCPKG_VISUAL_STUDIO_PATH', $vs2017Path)
 }
 
-## Check the Visual Studio 2022 PATH
-if (-Not (Test-Path -Path $vs2022Path)) { 
-    Write-Host "Path $vs2017Path does not exists!" -ForegroundColor Red 
-} else {
-    $vsPathExists = $true
-}
+$vsPath = [System.Environment]::GetEnvironmentVariable('VCPKG_VISUAL_STUDIO_PATH')
 
-if (-Not $vsPathExists) {
-    Write-Host "Please run the script passing a Visual Studio Path"
-    Write-Host ""
-    Write-Host "Example:"
+if (-Not $vsPath) {
+    Write-Host "No Visual Studio Path passed as environment variable"
+    Write-Host "Script could not work, Try with:"
     Write-Host ""
     Write-Host "bdp_make_all.ps1 -vs2022Path <VISUAL_STUDIO_2022_PATH> (preferred)"
     Write-Host "bdp_make_all.ps1 -vs2017Path <VISUAL_STUDIO_2017_PATH>"
-    Write-Host
-    Break
+    Write-Host ""
 }
 
-## Set the Visual Studio PATHs as environment variable
-[Environment]::SetEnvironmentVariable('VCPKG_VISUAL_STUDIO_PATH', $vs2022Path)
-[Environment]::SetEnvironmentVariable('VCPKG_VISUAL_STUDIO_PATH_VS2017', $vs2017Path)
-[Environment]::SetEnvironmentVariable('VCPKG_VISUAL_STUDIO_PATH_VS2022', $vs2022Path)
+#########################
+### VCPKG INSTALLATION
+#########################
 
 ## *** cpprestsdk ***
 .\vcpkg.exe install cpprestsdk:x86-windows-static
